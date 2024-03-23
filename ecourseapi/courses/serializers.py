02df93eab158d69f1d0a4109cf101c89b0c750a5
Ngân <nganbui.23112003@gmail.com>
2024-03-23 23:03:21 +0700

    custom serializer hình ảnh và tags

--- ecourseapi/courses/serializers.py
+++ ecourseapi/courses/serializers.py
@@ -5,9 +5,28 @@ from rest_framework import serializers
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields ="__all__"
-        
+        fields = "__all__"
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
+
+
 class CourseSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField(source='image')
+
+    tags = TagSerializer(many=True)
+    def get_image(self, course):
+        request = self.context.get('request')
+        if request:
+            # Thay vì truy cập vào thuộc tính name, bạn có thể sử dụng public_id hoặc url hoặc bất kỳ trường nào khác cần thiết từ đối tượng image
+            return f"https://res.cloudinary.com/dp0daqkme/image/upload/{course.image.public_id}.png"
+        return '/static/%s' % course.image.public_i
+
     class Meta:
         model = Course
         fields = "__all__"
+
+
+
