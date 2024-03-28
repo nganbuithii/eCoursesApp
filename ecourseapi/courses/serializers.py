465cced24444c90430412f4260ef189d38a99ad3
nganbuithii <nganbui.23112003@gmail.com>
2024-03-28 08:58:25 +0700

    fix đường dẫn ảnh

--- ecourseapi/courses/serializers.py
+++ ecourseapi/courses/serializers.py
@@ -12,29 +12,20 @@ class TagSerializer(serializers.ModelSerializer):
         model = Tag
         fields = ['id', 'name']
 class BaseSerializer(serializers.ModelSerializer):
-    image = serializers.SerializerMethodField(source='image')
+    #Sửa đường dẫn ảnh
+    def to_representation(self, instance):
+        req = super().to_representation(instance)
+        req['image'] = instance.image.url
+        return req
+    #image = serializers.SerializerMethodField(source='image')
     tags = TagSerializer(many=True)
 
 class CourseSerializer(BaseSerializer):
-
-    def get_image(self, course):
-        request = self.context.get('request')
-        if request:
-            # Thay vì truy cập vào thuộc tính name, bạn có thể sử dụng public_id hoặc url hoặc bất kỳ trường nào khác cần thiết từ đối tượng image
-            return f"https://res.cloudinary.com/dp0daqkme/image/upload/{course.image.public_id}.png"
-        return '/static/%s' % course.image.public_i
-
     class Meta:
         model = Course
         fields = "__all__"
 
 class LessonSerializer(BaseSerializer):
-    def get_image(self, lesson):
-        request = self.context.get('request')
-        if request:
-            # Tương tự như trong CourseSerializer, bạn có thể thay đổi logic xử lý hình ảnh ở đây
-            return f"https://res.cloudinary.com/dp0daqkme/image/upload/{lesson.image.public_id}.png"
-        return '/static/%s' % lesson.image.public_id
     class Meta:
         model = Lesson
         fields = "__all__"
