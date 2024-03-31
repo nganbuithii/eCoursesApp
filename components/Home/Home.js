16a4312447ef6db73436965293b9843564f60673
Ngân <nganbui.23112003@gmail.com>
2024-04-01 01:10:26 +0700

    Test component Home

---
+++ components/Home/Home.js
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native"
+import MyStyles from "../../Styles/MyStyles"
+import Styles from "./Styles"
+
+
+const Home = () => {
+    return (
+        <View style={MyStyles.container}>
+            <Text style={Styles.subjects}>HOME - Trang chủ</Text>
+        </View>
+    )
+    }
+
+export default Home
---
+++ components/Home/Styles.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native"
+
+export default StyleSheet.create({
+    subjects:{
+        fontSize:30,
+        color:"red",
+        fontWeight:"500"
+    }
+})
