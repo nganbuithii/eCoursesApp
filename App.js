7ff1778e98ac733b6c4e6b89715fb5b7eba40422
Ngân <nganbui.23112003@gmail.com>
2024-04-01 21:21:18 +0700

    fetch API get Category đổ ra nav

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import {NavigationContainer} from '@react-navigation/native'
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer';
+
 import Home from './components/Home/Home';
 import Login from './components/User/Login';
+import { useEffect, useState } from 'react';
+import API, { endpoints } from './Configs/API';
+import { ActivityIndicator } from 'react-native';
 
 const Drawer = createDrawerNavigator();
 
@@ -9,14 +14,53 @@ const Drawer = createDrawerNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Drawer.Navigator>
+      <Drawer.Navigator drawerContent={MyDrawerItem}>
         {/* gắn màn hình */}
+        {/* Nạp API Danh mục DrawerContent*/}
         <Drawer.Screen name="Home" component={Home} />
         <Drawer.Screen name='Login' component={Login} />
       </Drawer.Navigator>
     </NavigationContainer>
   )
 }
+
+const MyDrawerItem = ({ navigation, ...props }) => { // Destructure navigation từ props
+  //gọi API categories
+  const [categories, setCategories] = useState(null);
+
+  useEffect(() => {
+    const loadCategories = async() =>{
+      try{
+        let res = await API.get(endpoints['categories']);
+        setCategories(res.data)
+      } catch(ex) {
+        setCategories([])
+        console.error(ex);
+      }
+    }
+    loadCategories(); // Kích hoạt loadCategories khi component được mount
+  },[])
+
+  return (
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props}/>
+
+      {/* Kiểm tra nếu categories không null và không phải undefined */}
+      {categories !== null && categories !== undefined ? (
+        categories.map(c => (
+          <DrawerItem
+            key={c.id}
+            label={c.name}
+            onPress={() => navigation.navigate("Home", { "cateId": c.id })}
+          />
+        ))
+      ) : (
+        <ActivityIndicator /> // Hiển thị Indicator khi dữ liệu đang được tải
+      )}
+    </DrawerContentScrollView>
+  )
+}
+
 
 export default App;
 
