43b2d1013dd4d22c5b423067b8343b60ea9c6107
Ngân <nganbui.23112003@gmail.com>
2024-04-01 18:34:57 +0700

    gắn navigator

--- App.js
+++ App.js
@@ -1,39 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native'
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Home from './components/Home/Home';
+import Login from './components/User/Login';
 
-// import { NavigationContainer } from '@react-navigation/native';
-// import { createDrawerNavigator } from '@react-navigation/drawer';
-// import Login from './components/User/Login';
-// import Home from './components/Home/Home';
+const Drawer = createDrawerNavigator();
 
-// const Drawer = createDrawerNavigator();
-export default function App() {
+// Điều hướng trên màn hình hải bọc trong NavigationContainer
+const App = () => {
   return (
-    // <NavigationContainer>
-    //         <Drawer.Navigator initialRouteName='Home'>
-    //             <Drawer.Screen name="Home" component={Home} />
-    //             <Drawer.Screen name="Login" component={Login} />
-    //         </Drawer.Navigator>
-    // </NavigationContainer>
-    <View style={styles.container}>
-        <Text style={styles.txt}>HELLO WORLD</Text>
-        <StatusBar style="auto" />
-    </View>
-);
-
+    <NavigationContainer>
+      <Drawer.Navigator>
+        {/* gắn màn hình */}
+        <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen name='Login' component={Login} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  txt:{
-    color:"pink",
-    fontSize:50,
-    transform:[{rotate:"30deg"}]
-  }
-});
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+// import { StatusBar } from 'expo-status-bar';
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+
+// // import { NavigationContainer } from '@react-navigation/native';
+// // import { createDrawerNavigator } from '@react-navigation/drawer';
+// // import Login from './components/User/Login';
+// // import Home from './components/Home/Home';
+
+// // const Drawer = createDrawerNavigator();
+// export default function App() {
+//   return (
+//     // <NavigationContainer>
+//     //         <Drawer.Navigator initialRouteName='Home'>
+//     //             <Drawer.Screen name="Home" component={Home} />
+//     //             <Drawer.Screen name="Login" component={Login} />
+//     //         </Drawer.Navigator>
+//     // </NavigationContainer>
+//     <View style={styles.container}>
+//         <Text style={styles.txt}>HELLO WORLD</Text>
+//         <StatusBar style="auto" />
+//     </View>
+// );
+
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   txt:{
+//     color:"pink",
+//     fontSize:50,
+//     transform:[{rotate:"30deg"}]
+//   }
+// });
